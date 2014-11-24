e5d307dea195c0cbc87735d3304cea0dc9187032
bamgooly <alexknaack@gmail.com>
2014-11-24 11:36:50 -0800

    Remove unused code

--- test/test_crbf_training.py
+++ test/test_crbf_training.py
@@ -30,24 +30,6 @@ sub.call(crbfTrainCMD,shell=True,stdout=sys.stdout)
 # display results
 print('\x1B[34m==> \x1B[0m Plotting results')
 
-'''
-#load initiial random neural net yaml file
-yamlDoc = yaml.load(open('randomSpatioTemporalNeurons.yaml', 'r'))
-stn_0 = np.array(yamlDoc['spatio-temporal-neurons'][1:])
-fig1 = plt.figure()
-ax1 = fig1.add_subplot(1,1,1, projection='3d')
-ax1.scatter(stn_0[:,0],stn_0[:,1],stn_0[:,3])
-
-ax1.set_title('Randomized Spatio-temporal Neurons')
-ax1.set_xlabel('x')
-ax1.set_xlim3d(0, 1)
-ax1.set_ylabel('y')
-ax1.set_ylim3d(0, 1)
-ax1.set_zlabel('time')
-ax1.set_zlim3d(0, 2*np.pi)
-'''
-
-
 fig2 = plt.figure()
 ax2 = fig2.add_subplot(111, projection='3d')
 
@@ -57,46 +39,22 @@ ax2.set_ylabel('y')
 ax2.set_ylim3d(0, 1)
 ax2.set_zlabel('time')
 ax2.set_zlim3d(0, 2*np.pi)
-#ax2.set_zlabel('z')
-#ax2.set_zlim3d(0, 1)
 
 #load training trace date yaml file
 for yamlDoc in yaml.load_all(open('normalizedTrainingData.yaml', 'r')):
     ntd = np.array((yamlDoc['events'][1:]))
-    ax2.scatter(ntd[::150,0],ntd[::150,1],ntd[::150,3], c='g', edgecolor='c', marker='.', depthshade=False)
+    ax2.scatter(ntd[::100,0],ntd[::100,1],ntd[::100,3], c='g', edgecolor='c', marker='.', depthshade=False)
 
-'''
-#load 
-for yamlDoc in yaml.load_all(open('spatioTemporalTrain.yaml', 'r')):
-    stn = np.array((yamlDoc['spatio-temporal-neurons'][1:]))
-    ax2.scatter(stn[:,0],ntd[:,1],ntd[:,3], c='r', edgecolor='m', marker='o', depthshade=False)
-'''
 ims = []
 for yamlDoc in yaml.load_all(open('spatioTemporalTrain.yaml', 'r')):
     stn = np.array((yamlDoc['spatio-temporal-neurons'][1:]))
     scatter = ax2.scatter(stn[:,0],stn[:,1],stn[:,3], c='r', edgecolor='m', marker='o', depthshade=False)
     ims.append([scatter])
 
-ani = animation.ArtistAnimation(fig2, ims, interval=1000, repeat_delay=1000)
 
+ani = animation.ArtistAnimation(fig2, ims, interval=1000, repeat_delay=2000)
 
-'''
-
-#load trained neural net yaml file
-yamlDoc = yaml.load(open('crbfNeuralNet.yaml', 'r'))
-stn = np.array(yamlDoc['spatio-temporal-neurons'][1:])
-ax2.scatter(stn[:,0],stn[:,1],stn[:,3], c='r', edgecolor='m', depthshade=False)
-
-
-ax2.set_xlabel('x')
-ax2.set_xlim3d(0, 1)
-ax2.set_ylabel('y')
-ax2.set_ylim3d(0, 1)
-ax2.set_zlabel('time')
-ax2.set_zlim3d(0, 2*np.pi)
-'''
-
-
+ani.save('neural_training.mp4')
 
 
 plt.show()
