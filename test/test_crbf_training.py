3fc6ff0a31162633c69835b7f629d994307ceae0
bamgooly <alexknaack@gmail.com>
2014-12-17 15:58:33 -0800

    Add zidx var and other small cleanups

--- test/test_crbf_training.py
+++ test/test_crbf_training.py
@@ -25,8 +25,9 @@ if not args.plot:
     traceDataList = 'stc_1_list.txt'
 
     # check that build is up to date
-    #os.chdir(rootDir)
-    if sub.call(["make","-q","-C {}".format(rootDir)]):
+    makeCMD = "make -q -C '{}'".format(rootDir);
+    print(makeCMD)
+    if sub.call(makeCMD, shell=True):
         print('\x1B[33mwarning:\x1B[0m build not up to date')
 
     # run c-rbf training
@@ -43,21 +44,30 @@ if not args.train:
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
 
-    # Setting the axes properties
+    # set axes properties
+    ax.set_title('Spatio-temporal Layer Training')
+
     ax.set_xlabel('x')
     ax.set_xlim3d(0, 1)
 
     ax.set_ylabel('y')
     ax.set_ylim3d(0, 1)
 
-    ax.set_zlabel('time')
-    ax.set_zlim3d(0, 2*np.pi)
+    zidx = 2
+
+    if zidx == 3:
+        ax.set_zlabel('time')
+        ax.set_zlim3d(0, 2*np.pi)
+    elif zidx == 2:
+        ax.set_zlabel('z')
+        ax.set_zlim3d(0, 1)
+
 
     print('Plotting normalized training data')
     #load training trace date yaml file
     for yamlDoc in yaml.load_all(open('normalizedTrainingData.yaml', 'r')):
         ntd = np.array((yamlDoc['events'][1:]))
-        ax.scatter(ntd[::100,0],ntd[::100,1],ntd[::100,3], s=5, c='grey', edgecolor='paleturquoise', marker='o', depthshade=False, zorder=0, label='Training Data', alpha=0.8)
+        tdScatter = ax.scatter(ntd[::60,0],ntd[::60,1],ntd[::60,zidx], s=5, c='grey', edgecolor='paleturquoise', marker='o', depthshade=False, zorder=0, alpha=0.8)
 
     ims = []
 
@@ -66,13 +76,16 @@ if not args.train:
         stnw = np.array((yamlDoc['spatio-temporal-neuron-weights'][1:]))
         stnc = np.array((yamlDoc['spatio-temporal-neuron-edges'][1:]))
         links = np.array([vstack((stnw[cidx[0]], stnw[cidx[1]])).tolist() for cidx in stnc]) 
-        lc = art3d.Line3DCollection([[tuple(con[0,[0,1,3]]), tuple(con[1,[0,1,3]])] for con in links], lw=0.5 )
+        lc = art3d.Line3DCollection([[tuple(con[0,[0,1,zidx]]), tuple(con[1,[0,1,zidx]])] for con in links], lw=0.5 )
         lc.set_color('coral')
         lines = ax.add_collection(lc)
-        scatter = ax.scatter(stnw[:,0],stnw[:,1],stnw[:,3], s=25, c='crimson', edgecolor='coral', marker='o', depthshade=False)
-        ims.append([lines,scatter])
+        nScatter = ax.scatter(stnw[:,0],stnw[:,1],stnw[:,zidx], s=30, c='crimson', edgecolor='coral', marker='o', depthshade=False)
+        ims.append([lines,nScatter])
 
-    ani = animation.ArtistAnimation(fig, ims, interval=1000, repeat_delay=2000)
+
+    ax.legend([tdScatter, (lines, nScatter)] ,['Training Data','Spatio-temporal Neurons'])
+    
+    ani = animation.ArtistAnimation(fig, ims, interval=1500, repeat_delay=3000)
 
     #ani.save('neural_training.mp4')
 
