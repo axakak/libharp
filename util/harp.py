46a182b231856e22514aec2e1a08d46dae06846c
bamgooly <alexknaack@gmail.com>
2016-03-08 13:51:39 -0800

    Add auto file parser for plot (alpha)

--- util/harp.py
+++ util/harp.py
@@ -74,15 +74,59 @@ def eval(neural_net, trace):
 ###############################################################################
 # Plot Training output
 ###############################################################################
-def plot(train_list, st_neuron_train, zdata, gif, png, show):
+def td_arrayfromyaml(trace):
     import yaml
+    import numpy as np
+
+    skip = 200
+    yamlDoc = yaml.load(open(trace))
+
+    td = np.array((yamlDoc['events'][1::skip]))
+
+    if yamlDoc['coordinate-space'] != 'normalized':
+        tdMax = td.max(0)
+        tdMin = td.min(0)
+        tdScale = np.array([1,1,1,2*np.pi]) / (tdMax - tdMin)
+        td = (td[:,:] - tdMin) * tdScale
+
+    return td
+
+
+def plot(harp_files, zdata, gif, png, show):
+    import yaml
+    import os
     import numpy as np
     import matplotlib.pyplot as plt
     import matplotlib.animation as animation
     from mpl_toolkits.mplot3d import axes3d, art3d
 
+    trace_files = []
+    neural_net_files = []
+    train_file = []
+
+    print('\x1B[34m==> \x1B[0m Parsing File')
+
+    #TODO: if yaml files use an explicit file type key, parsing would be much simpler
+    for fname in harp_files:
+        print(fname)
+        f = open(fname)
+        if f.readline().startswith('%YAML'):# if YAML file
+            docCount = f.read().count('---')#count number of documents
+            f.seek(0) #return to start of stream
+            if docCount == 1:
+                yamlDoc = f.read(1000)
+                if 'events' in yamlDoc:
+                    trace_files.append(fname)
+                elif 'spatio-temporal-neuron-weights' in yamlDoc:# crbf neural net
+                    neural_net_files.append(fname)
+            else: #multi doc yaml file, assume st training
+                train_file.append(fname)
+        else: # assume text file
+            for trace in f.read().splitlines():
+                trace_files.append(trace)# add to trace data file list
+
     # display results
-    print('\x1B[34m==> \x1B[0m Plotting ')
+    print('\x1B[34m==> \x1B[0m Plotting')
 
     #setup 3D subplot
     x,y = plt.figaspect(.75)*1.5
@@ -90,7 +134,6 @@ def plot(train_list, st_neuron_train, zdata, gif, png, show):
     ax = fig.add_subplot(111, projection='3d')
 
     # set axes properties
-    ax.set_title('Spatio-temporal Layer Training')
     ax.view_init(elev=30, azim=-70)
 
     ax.set_xlabel('x')
@@ -113,60 +156,94 @@ def plot(train_list, st_neuron_train, zdata, gif, png, show):
         ax.set_zticks([0, np.pi, 2*np.pi])
         ax.set_zticklabels(['0', '$\pi$','2$\pi$'])
 
-    print('Plotting normalized training data in: {}'.format(train_list.name))
-    #load training trace date yaml file
-    trainList = train_list.read().splitlines()
-    skip = 250
-    for trace in trainList:
-        print(trace)
-        yamlDoc = yaml.load(open(trace))
-        td = np.array((yamlDoc['events'][1:]))
-        if yamlDoc['coordinate-space'] != 'normalized':
-            tdMax = td.max(0)
-            tdMin = td.min(0)
-            tdScale = np.array([1,1,1,2*np.pi]) / (tdMax - tdMin)
-            td = (td[::skip,:] - tdMin) * tdScale
-        tdScatter = ax.scatter(td[:,0],td[:,1],td[:,zidx],
-                               s=5, c='grey', edgecolor='paleturquoise',
-                               marker='o', depthshade=False, zorder=0,
-                               alpha=0.8)
+    leg_art = []
+    leg_lable = []
 
-    ims = []
+    if len(trace_files) > 0:
+        print('normalized trace data')
+        #load training trace date yaml file
+        skip = 200
+        for trace in trace_files:
+            print(trace)
+            td = td_arrayfromyaml(trace)
 
-    print('Plotting neural network and links in : {}'.format(st_neuron_train.name))
-    for yamlDoc in yaml.load_all(st_neuron_train):
-        stnw = np.array((yamlDoc['spatio-temporal-neuron-weights'][1:]))
-        stnc = np.array((yamlDoc['spatio-temporal-neuron-edges'][1:]))
-        links = np.array([np.vstack((stnw[cidx[0]], stnw[cidx[1]])).tolist()
-                                 for cidx in stnc])
+            tdScatter = ax.scatter(td[:,0],td[:,1],td[:,zidx],color=None)#,
+                                #    s=10, c='grey', edgecolor='paleturquoise',
+                                #    marker='o', depthshade=False, zorder=0,
+                                #    alpha=0.8)
 
-        lc = art3d.Line3DCollection([[tuple(con[0,[0,1,zidx]]),
-                                      tuple(con[1,[0,1,zidx]])] for con in links], lw=0.5)
+        leg_art.append(tdScatter)
+        leg_lable.append('Trace Data')
 
-        lc.set_color('coral')
-        lines = ax.add_collection(lc)
-        nScatter = ax.scatter(stnw[:,0],stnw[:,1],stnw[:,zidx],
-                              s=30, c='crimson', edgecolor='coral', marker='o',
-                              depthshade=False)
+    frames = []
 
-        ims.append([lines,nScatter])
+    if len(train_file) > 0:
+        print('Plotting neural network and links in...')
+        print(train_file[0])
+        for yamlDoc in yaml.load_all(open(train_file[0])):
+            stnw = np.array((yamlDoc['spatio-temporal-neuron-weights'][1:]))
+            stnc = np.array((yamlDoc['spatio-temporal-neuron-edges'][1:]))
+            links = np.array([np.vstack((stnw[cidx[0]], stnw[cidx[1]])).tolist()
+                                     for cidx in stnc])
+
+            lc = art3d.Line3DCollection([[tuple(con[0,[0,1,zidx]]),
+                                          tuple(con[1,[0,1,zidx]])] for con in links], lw=0.5)
+
+            lc.set_color('coral')
+            ntlines = ax.add_collection(lc)
+            ntScatter = ax.scatter(stnw[:,0],stnw[:,1],stnw[:,zidx],
+                                  s=30, c='crimson', edgecolor='coral', marker='o',
+                                  depthshade=False)
+
+            frames.append([ntlines,ntScatter])
 
 
-    legend = ax.legend([tdScatter, (lines, nScatter)],
-                       ['Training Data','Spatio-temporal Neurons'],
+    elif len(neural_net_files) > 0:
+        print('Plotting neural network and links...')
+        for neural_net in neural_net_files:
+            print(neural_net)
+            yamlDoc = yaml.load(open(neural_net))
+            stnw = np.array((yamlDoc['spatio-temporal-neuron-weights'][1:]))
+            stnc = np.array((yamlDoc['spatio-temporal-neuron-edges'][1:]))
+            links = np.array([np.vstack((stnw[cidx[0]], stnw[cidx[1]])).tolist()
+                                     for cidx in stnc])
+
+            lc = art3d.Line3DCollection([[tuple(con[0,[0,1,zidx]]),
+                                          tuple(con[1,[0,1,zidx]])] for con in links], lw=0.5)
+
+            lc.set_color('coral')
+
+            nLines = ax.add_collection(lc)
+            nScatter = ax.scatter(stnw[:,0],stnw[:,1],stnw[:,zidx],
+                                  s=30, c='crimson', edgecolor='coral', marker='o',
+                                  depthshade=False)
+        leg_art.append((nLines,nScatter))
+        leg_lable.append('Spatio-temporal Neurons')
+
+
+    legend = ax.legend(leg_art,leg_lable,
                        fontsize='medium', loc='lower right')
 
     legend.get_frame().set_edgecolor('darkgray')
 
-    ani = animation.ArtistAnimation(fig, ims, interval=1500, repeat_delay=3000)
+    # ax.set_title('Spatio-temporal Layer Training')
+
+    if len(frames) > 0:
+        ani = animation.ArtistAnimation(fig, frames, interval=1500, repeat_delay=3000)
 
     if gif:
-        ani.save('neural_training.gif', writer='imagemagick');
+        if len(frames) > 0:
+            ani.save('harp_plot.gif', writer='imagemagick');
+        else:
+            fig.savefig('harp_plot.gif')
 
     if png:
-        ani.save('neural_training.png', writer='imagemagick');
+        if len(frames) > 0:
+            ani.save('harp_plot.png', writer='imagemagick');
+        else:
+            fig.savefig('harp_plot.png')
 
-    if show:
+    if show or not (gif or png):
         print('Showing plot')
         plt.show()
 
@@ -228,12 +305,6 @@ if __name__ == "__main__":
     elif commandArgs.command == 'plot':
         plotParser = argparse.ArgumentParser("visualize the input and output of harp")
 
-        plotParser.add_argument('train_list',
-                                type=open)
-
-        plotParser.add_argument('st_neuron_train',
-                                type=open)
-
         plotParser.add_argument('-z','--zdata',
                                 action='store_true',
                                 help='Plot z spacial data on the z-axis')
@@ -247,7 +318,11 @@ if __name__ == "__main__":
         plotParser.add_argument('--show',
                                 action='store_true')
 
+        plotParser.add_argument('harp_files',
+                                nargs='+',
+                                help='One or more harp files to be plotted')
+
         plotArgs = plotParser.parse_args(commandArgs.args)
 
-        plot(plotArgs.train_list, plotArgs.st_neuron_train,
-             plotArgs.zdata, plotArgs.gif, plotArgs.png, plotArgs.show)
+        plot(plotArgs.harp_files, plotArgs.zdata,
+             plotArgs.gif, plotArgs.png, plotArgs.show)
