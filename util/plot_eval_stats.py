0dbd54a60a8b2ce396ddc4fba2084cb2a2a100df
bamgooly <alexknaack@gmail.com>
2016-04-11 14:21:53 -0700

    Fix eval stats header assignment

--- util/plot_eval_stats.py
+++ util/plot_eval_stats.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import argparse
 
+import scipy.stats as stats
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.mlab as mlab
@@ -16,15 +17,28 @@ parser.add_argument('file')
 
 args = parser.parse_args()
 
+header = open(args.file).readline().split(sep=',')
+h_names = []
+for name in header:
+    if name == 'neural net':
+        h_names.append('neural_net')
+    elif name == 'trace':
+        h_names.append('trace')
+    elif name == 'true class':
+        h_names.append('true_class')
+    elif '1' in name:
+        h_names.append('err_dom')
+    elif '0' in name:
+        h_names.append('err_non')
 
-evalStats = np.recfromcsv(args.file,skip_header=1,
-                        names=('neural_net','trace','true_class','err_non','err_dom'))
-
+evalStats = np.recfromcsv(args.file,skip_header=1,names=h_names)
 evalStats = append_fields(evalStats,'err_diff', (evalStats['err_non']-evalStats['err_dom']))
 
-#create 2 col array of dominant and nondominant error differences
-errDiff = hstack((vstack(evalStats[evalStats['true_class']%2 == 1]['err_diff']),
-           vstack(evalStats[evalStats['true_class']%2 == 0]['err_diff'])))
+#create arrayS of dominant and nondominant error differences
+errDiff_dom = vstack(evalStats[evalStats['true_class']%2 == 1]['err_diff'])
+errDiff_non = vstack(evalStats[evalStats['true_class']%2 == 0]['err_diff'])
+
+# print(hstack([errDiff_dom,errDiff_dom>0]))
 
 trace = evalStats['trace'][0].decode()[:6]
 
@@ -33,30 +47,70 @@ plt.rc('font', family = 'serif', serif = 'CMU Serif')
 ###############################################################################
 # Confusion matrix calculations
 ###############################################################################
-TP = len(errDiff[errDiff[:,0]>0,0]) #true positive
-FN = len(errDiff[errDiff[:,0]<0,0]) #false negative
-FP = len(errDiff[errDiff[:,1]>0,1]) #false positive
-TN = len(errDiff[errDiff[:,1]<0,1]) #true negative
+N = len(evalStats)
+TP = len(errDiff_dom[errDiff_dom>0]) # true positive (A)
+FN = len(errDiff_dom[errDiff_dom<0]) # false negative (C)
+FP = len(errDiff_non[errDiff_non>0]) # false positive (B)
+TN = len(errDiff_non[errDiff_non<0]) # true negative (D)
 
-TPR = TP/(TP+FN)
-TNR = TN/(FP+TN)
+PP = TP+FP # predicted positives
+PN = FN+TN # predicted negatives
+RP = TP+FN # real positives
+RN = FP+TN # real negatives
 
-PPV = TP/(TP+FP)
-NPV = TN/(FN+TN)
+bias = PP/N # label bias
+prev = RP/N # population prevalence
 
+prevG2 = prev*(1-prev)
+biasG2 = bias*(1-bias)
 
-print('True positive: {}'.format(TP))
-print('False negative: {}'.format(FN))
+TPR = TP/(TP+FN) # true positive rate, sensitivity, recall
+TNR = TN/(FP+TN) # true negative rate, specificity, inverse recall
 
-print('False positive: {}'.format(FP))
-print('True negative: {}'.format(TN))
+PPV = TP/(TP+FP) # positive predictive value, precision
+NPV = TN/(FN+TN) # negative predictive value, inverse precision
 
-print('True positive rate (sensitivity): {}'.format(TPR))
-print('True negative rate (specificity): {}'.format(TNR))
+informedness = TPR+TNR-1 # DeltaP'
+markedness =  PPV+NPV-1 # DeltaP
 
-print('Positive predictive value: {:.2f}'.format(PPV))
-print('Negative predictive value: {:.2f}'.format(NPV))
+correlation = (TP*TN - FP*FN) / sqrt((TP+FN)*(FP+TN)*(TP+FP)*(FN+TN))
 
+accuracy = (TP+TN) / N
+
+print('\t+R\t-R')
+print('+P\t{}\t{}\t{}'.format(TP,FP,PP))
+print('-P\t{}\t{}\t{}'.format(FN,TN,PN))
+print('')
+print('\t{}\t{}\t{}'.format(RP,RN,N))
+print('')
+
+print('Informedness: {:.4f}'.format(informedness))
+print('Markedness: {:.4f}'.format(markedness))
+print('Correlation: {:.4f}'.format(correlation))
+print('')
+
+print('Bias: {:.4f}'.format(bias))
+print('Prevalence: {:.4f}'.format(prev))
+
+print('BiasG2: {:.4f}'.format(biasG2))
+print('PrevG2: {:.4f}'.format(prevG2))
+print('')
+
+print('Recall: {:.4f}'.format(TPR))
+print('Precision: {:.4f}'.format(PPV))
+print('IRecall: {:.4f}'.format(TNR))
+print('IPrecision: {:.4f}'.format(NPV))
+print('')
+
+print('Accuracy: {:.4f}'.format(accuracy))
+print('')
+
+obs = np.array([[TP, FP], [FN, TN]])
+
+oddsratio, pvalue = stats.fisher_exact(obs)
+print('Fisher exact pvalue: {:.8f}'.format(pvalue))
+print('Fisher exact odds: {:.8f}'.format(oddsratio))
+print('')
 
 ###############################################################################
 # Box Plot
@@ -76,7 +130,7 @@ ax.set_ylabel('Class Error Difference $(c_{non}-c_{dom})$')
 ax.axhline(y=0, color='gray')
 
 labels = [('Dominant'),('Nondominant')]
-ax.boxplot(errDiff,labels=labels)
+ax.boxplot([errDiff_dom,errDiff_non],labels=labels)
 
 ymax = max([abs(y) for y in ax.get_ylim()])
 
